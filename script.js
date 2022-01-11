d5eb2358f968e417c4f1837dce3e4d38fca70c4a
inayarajwani <inaya.rajwani@gmail.com>
2022-01-10 20:37:28 -0500

    new changes

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
+// $(document).ready(function(){
+//     $(window).scrollTop(){
+//         if (this.scrollY > 20){
+//             $('.navibar').addClass('sticky');
+//         } else {
+//             $('.navibar').removeClass('sticky'); 
+//         }
+//     };
+//     //toggle menu/navbar script
+//     $('.menu-tog').click(function(){
+//         $('.navibar .menu').toggle("active");
+//         $('.menu-tog i').toggle("active");
+//     });
+//     //typing animation script
+//     var typed = new Typed(".typing-txt", {
+//         strings: ["technology", "making a positive impact on day-to-day life and giving back to the community", "exploring my creativity through fashion and photography"],
+//         typingSpeed:100,
+//         deleteSpeed:60,
+//         loop: true
+//     });
+// });
+
 $(document).ready(function(){
-    $(window).scrollTop(){
-        if (this.scrollY > 20){
+    $(window).scroll(function(){
+        console.log(window.scrollY)
+        if (window.scrollY >= 60){
             $('.navibar').addClass('sticky');
         } else {
             $('.navibar').removeClass('sticky'); 
         }
-    };
+    });
+
     //toggle menu/navbar script
     $('.menu-tog').click(function(){
+        console.log("hello")
         $('.navibar .menu').toggle("active");
         $('.menu-tog i').toggle("active");
     });
@@ -15,11 +40,10 @@ $(document).ready(function(){
     var typed = new Typed(".typing-txt", {
         strings: ["technology", "making a positive impact on day-to-day life and giving back to the community", "exploring my creativity through fashion and photography"],
         typingSpeed:100,
-        deleteSpeed:60,
+        deleteSpeed:400,
         loop: true
     });
 });
-
 
 /*$(document).ready(function(){
     $(window).scroll(function(){
